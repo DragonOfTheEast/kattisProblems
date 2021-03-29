f6d0cbbfb2e2940d16fbe55f37095204b57005be
DragonOfTheEast <49605302+DragonOfTheEast@users.noreply.github.com>
2021-03-29 18:35:31 -0500

    Add files via upload

---
+++ shortestpath1.py
@@ -0,0 +1,32 @@
+from collections import deque
+from math import isinf, inf
+def solve(graph, s, n):
+    dist = [inf]*(n+1)
+    dist[s] = 0
+    queue = deque()
+    queue.append(s)
+    while queue:
+        current = queue.popleft()
+        for j in graph[current]:
+            node, weight = j[0], j[1]
+            if dist[node] > dist[current] + weight:
+                dist[node] = dist[current] + weight
+                queue.append(node)
+    return dist
+
+while True:
+    n,m,q,s = [int(i) for i in input().split()]
+    if n == m == q == s == 0:
+        break
+    graph = [[] for i in range(n+1)]
+    for i in range(m):
+        u, v, w = [int(i) for i in input().split()]
+        graph[u].append([v, w])
+    ans = solve(graph, s, n)
+    for i in range(q):
+        v = int(input())
+        if isinf(ans[v]):
+            print("impossible")
+        else:
+            print(ans[v])
+    print()
